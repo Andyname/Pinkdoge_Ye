dd7692f266247d98b3e8f6996dc1d8e58f7b4f37
Andyname <3361534564@qq.com>
2019-08-31 09:52:32 +0000

    xpe-ify

--- xpe_kiwi.mk
+++ xpe_kiwi.mk
@@ -15,9 +15,11 @@
 $(call inherit-product, device/huawei/kiwi/full_kiwi.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/xperience/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_kiwi
+PRODUCT_NAME := xpe_kiwi
 BOARD_VENDOR := huawei
 
 PRODUCT_GMS_CLIENTID_BASE := android-huawei
+TARGET_BOOT_ANIMATION_RES := 1080
+export XPE_BUILD_TYPE=Pinkdoge
